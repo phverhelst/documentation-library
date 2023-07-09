acf7fd175052f1adc3807fe730d34f3b5e26c9d0
phverhelst <115952399+phverhelst@users.noreply.github.com>
2023-07-09 23:48:27 +0200

    update

---
+++ CraftingThreeGuiAppsWithPythonTkinter/converter.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("Metric Converter")
+
+root.mainloop()
--- CraftingThreeGuiAppsWithPythonTkinter/notepad.py
+++ CraftingThreeGuiAppsWithPythonTkinter/notepad.py
@@ -1,9 +1,66 @@
 import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+from tkinter import *
+from tkinter import filedialog, messagebox
+
 
 root  = tk.Tk()
 
 
 root.title("Notes")
+
+textarea = ScrolledText(root)
+textarea.pack()
+
+menu = Menu(root)
+file = Menu(menu, tearoff = 0)
+
+def Open():
+    root.filename = filedialog.askopenfilename(
+        initialdir = "/",
+        title = "Select File",
+        filetypes = (
+            ("txt files", "*.txt"),
+            ("all files", "*.*")
+            )
+        )
+    file = open(root.filename)
+    text.insert("end", file.read())
+
+def  Save():
+    pass
+
+def SaveAs():
+    root.filename = filedialog.asksaveasfile(
+        mode = "w",
+        defaultextension = ".txt"
+    )
+    if root.filename is None:
+        return
+    file_to_save = str(text.get  (1.0, END))
+    root.filename.write(file_to_save)
+    root.filename.close()
+
+def Exit():
+    message = messagebox.askquestion("Notepad", "Do you want to save?")
+    if message == "yes":
+        SaveAs()
+    else:
+        root.destroy()
+
+
+
+file.add_command(label = "Open", command = Open)
+file.add_command(label = "Save", command = Save)
+file.add_command(label = "Save As", command = SaveAs)
+file.add_separator()
+file.add_command(label = "Exit", command = Exit)
+
+menu.add_cascade(label = "File", menu = file)
+
+
+root.config(menu = menu)
+
 
 
 root.mainloop()
