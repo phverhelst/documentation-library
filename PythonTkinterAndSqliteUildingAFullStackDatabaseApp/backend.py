5313686fb59ee341c9cf4c36391bef89da000369
phverhelst <115952399+phverhelst@users.noreply.github.com>
2023-07-14 18:18:55 +0200

    update

--- PythonTkinterAndSqliteUildingAFullStackDatabaseApp/backend.py
+++ PythonTkinterAndSqliteUildingAFullStackDatabaseApp/backend.py
@@ -2,19 +2,30 @@ import sqlite3
 
 def connect():
 
-    connection = sqlite3.connect("courses.db")
+    connection = sqlite3.connect("PythonTkinterAndSqliteUildingAFullStackDatabaseApp\courses.db")
     cursor = connection.cursor()
-    cursor.execute('''
-                   CREATE TABLE IF NOT EXISTS course
-                   (
-                   id INTEGER PRIMARY KEY,
-                   name TEXT,
-                   category TEXT
-                   )
-                   '''
-    )
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS course
+            (id INTEGER PRIMARY KEY,
+            name TEXT,
+            category TEXT,
+            author TEXT,
+            price TEXT
+            )''')
 
     connection.commit()
     connection.close()
 
+def create(name, category, author, price):
+    connection = sqlite3.connect("PythonTkinterAndSqliteUildingAFullStackDatabaseApp\courses.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO course VALUES (NULL, ?, ?, ?, ?)",  (name, category, author, price))
+    connection.commit()
+    connection.close()
+
+
+
 connect()
+
+create("Hello Coding", "programming", "Mammoth Interactive", "99.99")
